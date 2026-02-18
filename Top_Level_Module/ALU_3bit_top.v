1bc78ff72404d6c594ef304e0339b2edb9d86f57
antoniov$502 <bryanvasquez797@gmail.com>
2026-02-18 13:28:57 -0800

    Update ALU_3bit_top.v

--- Top_Level_Module/ALU_3bit_top.v
+++ Top_Level_Module/ALU_3bit_top.v
@@ -1,9 +1,19 @@
 module ALU_3bit_top(SW, LEDR);
-    input  [9:0] SW;         // SW[9:0] switches
-    output [9:0] LEDR;      // LEDR[9:0] red LEDs
+input  [9:0] SW;         // SW[9:0] switches
+output [9:0] LEDR;      // LEDR[9:0] red LEDs
 
-ALU_3bit U1 (.a(SW[2:0]), .b(SW[5:3]), .sel(SW[8:6]), .result(LEDR[2:0]), .carry_out(LEDR[3]), 
-             .zero(LEDR[4]), .equal(LEDR[5]), .less_than(LEDR[6]), .greater_than(LEDR[7]));
+assign LEDR [9:8] = 2'b00;
+//SW[8] not used   
+    
+ALU_3bit U1 (.a(SW[5:3]), 
+             .b(SW[2:0]), 
+             .sel(SW[9:7]), 
+             .result(LEDR[2:0]), 
+             .carry_out(LEDR[3]), 
+             .zero(LEDR[4]), 
+             .equal(LEDR[5]), 
+             .less_than(LEDR[6]), 
+             .greater_than(LEDR[7]));
 
 
 endmodule
