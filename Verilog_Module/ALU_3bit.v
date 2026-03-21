b13884389915705b64435ccafd30ea31d007a437
antoniov$502 <bryanvasquez797@gmail.com>
2026-03-21 14:57:50 -0700

    Update ALU_3bit.v

--- Verilog_Module/ALU_3bit.v
+++ Verilog_Module/ALU_3bit.v
@@ -28,54 +28,52 @@ module ALU_3bit(A, B, sel, result, carry_out, zero, equal, less_than, greater_th
    greater_than = 0;   //initializing greater_than to 0
    
     case (sel)
-
-	XOR: begin
-		result = A ^ B;  //bitwsie XOR operation
-	end
+		XOR: begin
+			result = A ^ B;  //bitwsie XOR operation
+		end
+			
+	    ADD: begin 
+	      {carry_out, result} = A + B; //carryout if result exceeds 3 bits
+	    end
+	     
+	    SUB: begin
+	      result = A - B;
+	      carry_out = (A < B) ? 1 : 0; //carryout acts as borrowout
+	    end
+	    
+	    AND: begin
+	      result = A & B;         //bitwise AND operation
+	    end
+	    
+	    OR: begin 
+	      result = A | B;  //bitwise OR operation
+	    end
 		
-    ADD: begin 
-      {carry_out, result} = A + B; //carryout if result exceeds 3 bits
-    end
-     
-    SUB: begin
-      result = A - B;
-      carry_out = (A < B) ? 1 : 0; //carryout acts as borrowout
-    end
-    
-    AND: begin
-      result = A & B;         //bitwise AND operation
-    end
-    
-    OR: begin 
-      result = A | B;  //bitwise OR operation
-    end
-	
-    EQ: begin 
-      equal = (A == B) ? 1 : 0;  //equal output goes high if A and B are equal, else 0
-    
-    end
-     
-    LT: begin 
-      less_than = (A < B) ? 1: 0; //less_than output goes high if A is less than B, else 0
-    end 
-     
-     GT: begin 
-       greater_than = (A > B) ? 1 : 0;  //greater_than output goes high if A is greater than B, else 0
-     end 
-     
-    default: begin
-      result = 3'b000; //3 bit zero
-      carry_out = 0;
-	  zero = 0;
-	  equal = 0;
-      less_than = 0;
-      greater_than = 0;
-    end
-  endcase
+	    EQ: begin 
+	      equal = (A == B) ? 1 : 0;  //equal output goes high if A and B are equal, else 0
+	    
+	    end
+	     
+	    LT: begin 
+	      less_than = (A < B) ? 1: 0; //less_than output goes high if A is less than B, else 0
+	    end 
+	     
+	     GT: begin 
+	       greater_than = (A > B) ? 1 : 0;  //greater_than output goes high if A is greater than B, else 0
+	     end 
+	     
+	    default: begin
+	      result = 3'b000; //3 bit zero
+	      carry_out = 0;
+		  zero = 0;
+		  equal = 0;
+	      less_than = 0;
+	      greater_than = 0;
+	    end
+	endcase
 
   
 	  if ((result == 3'b000) && (carry_out == 0) && (sel != EQ) && (sel != LT) && (sel != GT)) 
         zero = 1; // zero only goes high for arithemtic operations and logical operations and if carry out is 0 and result is zero
-      
-end
+ end
 endmodule
